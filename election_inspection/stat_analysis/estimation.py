e5c2060db06e27477c1e32992d18e485c9dee3af
Victor <victor@Victors-MacBook-Air.local>
2022-03-16 14:09:57 -0500

    updated stat_analysis

--- election_inspection/stat_analysis/estimation.py
+++ election_inspection/stat_analysis/estimation.py
@@ -15,10 +15,10 @@ import estimation_util
 clean = pd.read_csv('working_dataset.csv', index_col = 0)
 
 # Define the index position of the explanatory variables
-X_cols = list(range(3,23))
+X_cols = list(range(1,20))
 
 # Define the index position of the dependent variable
-y_col = 23
+y_col = 20
 
 best_X_cols = estimation_util.forward_selection_AIC(clean, y_col, X_cols)
 X = estimation_util.get_X(clean, best_X_cols)
@@ -34,16 +34,19 @@ def write_prediction_csv(betas, X_cols, map_name, clean_df):
     map = pd.read_csv('election_inspection/stat_analysis/' + map_name + '_vtd_joined.csv',
                        index_col = 0)
     clean_df = clean_df.join(map)
-    sums = clean_df.loc[:, ['DISTRICT', 'total_pop', 'pop_perc_white', 'c_perc_hs_grad', 'turnout2018_registered', 'c_pop_pct_urban']]
-    sums = sums.groupby(['DISTRICT']).sum()
     means = clean_df.groupby(['DISTRICT']).mean()
-    X = estimation_util.get_X(collapsed, X_cols)
+    X = estimation_util.get_X(means, X_cols)
     means['predicted_turnout'] = estimation_util.predict(betas, X)
-    results = means.join(sums)
-    results.to_csv('election_inspection/stat_analysis/' + map_name + '_results.csv')
+    means.to_csv('election_inspection/stat_analysis/' + map_name + '_results.csv')
 
 
 # Define the filenames of the files for each map
 map_names = ['apple', 'birch', 'chestnut', 'lange', 'szetela']
 for name in map_names:
     write_prediction_csv(betas, best_X_cols, name, clean)
+
+
+# Create the results at the vtd for visualization
+X = estimation_util.get_X(clean, best_X_cols)
+clean['predicted_turnout'] = estimation_util.predict(betas, X)
+clean.to_csv('election_inspection/stat_analysis/vtd_results.csv')
