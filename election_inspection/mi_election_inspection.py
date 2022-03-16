a524ff3b987df1054fda472e33b46174d4488660
Me <90344922+chrjor@users.noreply.github.com>
2022-03-16 14:32:45 -0500

    script update

--- election_inspection/mi_election_inspection.py
+++ election_inspection/mi_election_inspection.py
@@ -1,8 +1,35 @@
 import argparse 
 from data_cleaning.import_data import process_data
-#from stat_analysis import ...
-#from visual_analysis import ...
+from stat_analysis.match_vtd_to_districts import run_matching, csv_joined_files
+from visual_analysis.dash_map import
 
 def run_analysis():
-    i = input('input:')
+    '''
+    Main function to run election inspection analysis.
+    '''
+
+    # Run script to collect and clean data
+    process_data()
+    
+    print("Cleaned csv file outputted to 'data_cleaning/data' directory.\n")
+    input('Press any key to continue...')
+
+    # Run scripts to perform analysis
+    run_matching()
+    print("Created mapping of vtds to new districts and output csvs to 'stat_analysis/' directory.\n")
+    input('Press any key to continue...')
+
+
+
+
+
+    exec('/visual_analysis/dash_map.py')
+
+
+
+
+
+
+
+
 
