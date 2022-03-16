0209da4fb25b890aa06d164b14dad77c8b7d2490
Victor <victor@Victors-MacBook-Air.local>
2022-03-16 11:29:01 -0500

    Updated estimation.py

--- election_inspection/stat_analysis/estimation.py
+++ election_inspection/stat_analysis/estimation.py
@@ -12,17 +12,9 @@ Last modified: 15/03/2021
 import pandas as pd
 import estimation_util
 
-
 clean = pd.read_csv('working_dataset.csv')
-
-apple = pd.read_csv('data/csv/apple_vtd_joined.csv')
-birch = pd.read_csv('data/csv/birch_vtd_joined.csv')
-chestnut = pd.read_csv('data/csv/chestnut_vtd_joined.csv')
-lange = pd.read_csv('data/csv/lange_vtd_joined.csv')
-szetela = pd.read_csv('data/csv/szetela_vtd_joined.csv')
-
-df.join(other.set_index('key'), on='key')
-clean.join(apple.set_index('GEOID20'), on = 'GEOID20')
+clean['GEOID20'] = clean['GEOID20'].astype('int')
+clean['DISTRICT'] = clean['DISTRICT'].astype('int')
 
 # Define the index position of the explanatory variables
 X_cols = list(range(5,22))
@@ -35,27 +27,29 @@ X = estimation_util.get_X(clean, best_X_cols)
 y = estimation_util.get_y(clean, y_col)
 betas = estimation_util.regress(X, y)
 
-
-collapsed = df.groupby(['DISTRICT']).mean()
-X = estimation_util.get_X(collapsed, best_X_cols)
-collapsed['predicted_turnout'] = estimation_util.predict(betas, X)
-collapsed.to_csv(filename)
-
-
-# Define the filenames of the files for each map
-filenames = ['apple', 'birch', 'chestnut', 'lange', 'szetela']
-for file in filenames:
-    write_prediction_csv(betas, best_X_cols, file)
-
-
-def write_prediction_csv(betas, X_cols, filename):
+def write_prediction_csv(betas, X_cols, map_name, clean_df):
     '''
     Concatenates the predicted values to a dataframe
     and exports the results as a csv
     '''
-    input_csv = 'data/csv/' + filename + '.csv'
-    output_csv = 'data/csv/' + filename + '_results.csv'
-    prediction_data = pd.read_csv(input_csv)
-    X = estimation_util.get_X(prediction_data, X_cols)
-    prediction_data['predicted_turnout'] = estimation_util.predict(betas, X)
-    prediction_data.to_csv(output_csv)
+    map = pd.read_csv('stat_analysis/' + map_name + '_vtd_joined.csv')
+    clean_df.join(map.set_index('GEOID20'), on = 'GEOID20')
+    collapsed = clean_df.groupby(['DISTRICT']).mean()
+    X = estimation_util.get_X(collapsed, X_cols)
+    collapsed['predicted_turnout'] = estimation_util.predict(betas, X)
+    collapsed.to_csv('stat_analysis/' + map_name + '_results.csv')
+
+
+# Define the filenames of the files for each map
+map_names = ['apple', 'birch', 'chestnut', 'lange', 'szetela']
+for name in map_names:
+    write_prediction_csv(betas, best_X_cols, name, clean)
+
+
+# apple = pd.read_csv('stat_analysis/apple_vtd_joined.csv')
+# birch = pd.read_csv('stat_analysis/birch_vtd_joined.csv')
+# chestnut = pd.read_csv('stat_analysis/chestnut_vtd_joined.csv')
+# lange = pd.read_csv('stat_analysis/lange_vtd_joined.csv')
+# szetela = pd.read_csv('stat_analysis/szetela_vtd_joined.csv')
+
+
